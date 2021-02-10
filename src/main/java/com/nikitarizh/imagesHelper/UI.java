2d23c6df2eba8fe85fe8c3e76604c05bc08de6e7
nikitarizh <nrigworks@gmail.com>
2021-02-11 00:38:26 +0300

    feat: UI update

--- src/main/java/com/nikitarizh/imagesHelper/UI.java
+++ src/main/java/com/nikitarizh/imagesHelper/UI.java
@@ -2,6 +2,7 @@ package com.nikitarizh.imagesHelper;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.nikitarizh.imagesHelper.helpers.FileHelper;
@@ -29,12 +30,31 @@ public class UI {
         System.out.println("\n\nSpecify new width");
         int newWidth = in.nextInt();
 
+        // new height input
+        System.out.println("\n\nSpecify new height or print any letter to skip");
+        int newHeight = -1;
+        try {
+            newHeight = in.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Height will be auto abjusted");
+        }
+
         // new name
         // if old name was image.jpg|png, new name will be image-processed.jpg
         String newName = filename.replaceAll("([.][j][p][g])|([.][p][n][g])", "") + "-processed";
 
+        // resizing image
+        BufferedImage resizedImage;
+        if (newHeight == -1) {
+            resizedImage = ImageHelper.resizeImage(image, newWidth, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving());
+        }
+        else {
+            resizedImage = ImageHelper.resizeImage(image, newWidth, newHeight, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving());
+        }
+
         // writing resized image
-        FileHelper.writeImage(newName, "jpg", ImageHelper.resizeImage(image, newWidth, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving()));
+        FileHelper.writeImage(newName, "jpg", resizedImage);
 
         // closing scanner
         in.close();
