003bb7ed14cf46e5260516e73461ba8e422ba18e
nikitarizh <nrigworks@gmail.com>
2021-02-10 21:31:39 +0300

    feat: common interface for Blur algorithms (Blur strategy)

---
+++ src/main/java/com/nikitarizh/imagesHelper/blur/Blur.java
@@ -0,0 +1,22 @@
+package com.nikitarizh.imagesHelper.blur;
+
+import java.awt.image.BufferedImage;
+
+public class Blur implements Blurrer {
+
+    private Blurrer blurAlgorithm;
+
+    public Blur(Blurrer blurrer) {
+        this.blurAlgorithm = blurrer;
+    }
+
+    @Override
+    public BufferedImage blur(BufferedImage image, int radius) {
+        return blurAlgorithm.blur(image, radius);
+    }
+
+}
+
+interface Blurrer {
+    public BufferedImage blur(BufferedImage image, int radius);
+}
