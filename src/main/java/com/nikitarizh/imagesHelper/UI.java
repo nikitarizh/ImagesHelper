3c46b655ad4717142a4849171900343b0e7dbe9e
nikitarizh <nrigworks@gmail.com>
2021-02-16 10:42:14 +0300

    feat: process all images in specified directory

--- src/main/java/com/nikitarizh/imagesHelper/UI.java
+++ src/main/java/com/nikitarizh/imagesHelper/UI.java
@@ -2,6 +2,8 @@ package com.nikitarizh.imagesHelper;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -16,14 +18,36 @@ public class UI {
         final Scanner in = new Scanner(System.in);
 
         // filename input
-        System.out.println("\nSpecify initial filename");
+        System.out.println("\nSpecify initial filename or directory with images");
         String filename = in.nextLine();
+        File file = FileHelper.getFile(filename);
 
-        // trying to get image with input filename
-        BufferedImage image = FileHelper.getImage(filename);
-        if (image == null) {
-            App.logger.error("Image is not loaded");
-            System.exit(1);
+        ArrayList<File> files = new ArrayList<>();
+        if (file.isDirectory()) {
+            for (final File entry : file.listFiles()) {
+                if (!entry.isDirectory()) {
+                    files.add(entry);
+                }
+            }
+        }
+        else {
+            files.add(file);
+        }
+
+        ArrayList<BufferedImage> imageList = new ArrayList<>();
+        ArrayList<File> errorFiles = new ArrayList<>();
+
+        for (final File entry : files) {
+            // trying to get image with input filename
+            BufferedImage image = FileHelper.getImage(entry);
+            if (image == null) {
+                App.logger.error("Image is not loaded");
+                errorFiles.add(entry);
+                imageList.add(null);
+            }
+            else {
+                imageList.add(image);
+            }
         }
 
         // new width input
@@ -40,21 +64,51 @@ public class UI {
             System.out.println("Height will be auto abjusted");
         }
 
-        // new name
-        // if old name was image.jpg|png, new name will be image-processed.jpg
-        String newName = filename.replaceAll("([.][j][p][g])|([.][p][n][g])", "") + "-processed";
+        int index = -1;
+        for (BufferedImage entry : imageList) {
+            index++;
+            if (entry == null) {
+                continue;
+            }
 
-        // resizing image
-        BufferedImage resizedImage;
-        if (newHeight == -1) {
-            resizedImage = ImageHelper.resizeImage(image, newWidth, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving());
-        }
-        else {
-            resizedImage = ImageHelper.resizeImage(image, newWidth, newHeight, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving());
+            App.logger.debug("***********************");
+            App.logger.debug("Processing image " + entry);
+            App.logger.debug("***********************");
+            // new name
+            // if old name was image.jpg|png, new name will be image-processed.jpg
+            String newName = files.get(index).getName().replaceAll("([.][j][p][g])|([.][p][n][g])", "") + "-processed";
+
+            // resizing image
+            BufferedImage resizedImage;
+            if (newHeight == -1) {
+                resizedImage = ImageHelper.resizeImage(entry, newWidth, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving());
+            }
+            else {
+                resizedImage = ImageHelper.resizeImage(entry, newWidth, newHeight, Image.SCALE_SMOOTH, new ResizeWithBlurRatioSaving());
+            }
+
+            if (resizedImage != null) {
+                // writing resized image
+                FileHelper.writeImage(newName, "jpg", resizedImage);
+                App.logger.info("Successfully processed image " + entry);
+            }
+            else {
+                errorFiles.add(files.get(index));
+            }
         }
 
-        // writing resized image
-        FileHelper.writeImage(newName, "jpg", resizedImage);
+        int success = files.size() - errorFiles.size();
+
+        if (success > 0) {
+            App.logger.info("Successfully processed " + success + " images");
+        }
+        
+        if (errorFiles.size() > 0) {
+            App.logger.error("Aborted " + errorFiles.size() + " images");
+            for (File f : errorFiles) {
+                App.logger.error(f.getPath());
+            }
+        }
 
         // closing scanner
         in.close();
